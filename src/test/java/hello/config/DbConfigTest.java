6bc5f0f9780d867e8fef3eaadf5778bef36dc522
bgm <qkrrudals2403@naver.com>
2023-11-02 15:45:23 +0900

    feat : 자동 구성 직접 만들기 - 기반 예제

--- src/test/java/hello/config/DbConfigTest.java
+++ src/test/java/hello/config/DbConfigTest.java
@@ -1,7 +1,6 @@
 package hello.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,7 +10,6 @@ import org.springframework.transaction.TransactionManager;
 import javax.sql.DataSource;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 @SpringBootTest
